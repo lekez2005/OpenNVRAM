5649553b97d3a3fd47381019712232dc5aa14c1c
ota2 <ota2@cornell.edu>
2018-11-19 01:13:43 -0500

    Re-add commented tests

--- compiler/tests/cam/06_predecoder_with_flops_test.py
+++ compiler/tests/cam/06_predecoder_with_flops_test.py
@@ -11,20 +11,20 @@ from globals import OPTS
 class PredecoderFlops(CamTestBase):
 
 
-    # def test_without_flop(self):
-    #
-    #     from modules.hierarchical_predecode3x8 import hierarchical_predecode3x8 as pre3x8
-    #
-    #     debug.info(2, "Testing 3x8 predecoder without flops")
-    #     decoder = pre3x8(route_top_rail=True, use_flops=False)
-    #     self.local_check(decoder)
-    #
-    # def test_with_flop(self):
-    #     from modules.hierarchical_predecode3x8 import hierarchical_predecode3x8 as pre3x8
-    #
-    #     debug.info(2, "Testing 3x8 predecoder without flops")
-    #     decoder = pre3x8(route_top_rail=True, use_flops=True)
-    #     self.local_check(decoder)
+    def test_without_flop(self):
+
+        from modules.hierarchical_predecode3x8 import hierarchical_predecode3x8 as pre3x8
+
+        debug.info(2, "Testing 3x8 predecoder without flops")
+        decoder = pre3x8(route_top_rail=True, use_flops=False)
+        self.local_check(decoder)
+
+    def test_with_flop(self):
+        from modules.hierarchical_predecode3x8 import hierarchical_predecode3x8 as pre3x8
+
+        debug.info(2, "Testing 3x8 predecoder without flops")
+        decoder = pre3x8(route_top_rail=True, use_flops=True)
+        self.local_check(decoder)
 
     def test_full_decoder_with_flops(self):
         from modules.hierarchical_decoder import hierarchical_decoder

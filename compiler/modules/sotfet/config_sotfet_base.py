0726f851aa1b152b77d9d32f77bf1285907c4f87
ota2 <lekez2005@yahoo.ca>
2019-08-12 16:56:09 -0400

    Update sotfet cam base cells and sizing

--- compiler/modules/sotfet/config_sotfet_base.py
+++ compiler/modules/sotfet/config_sotfet_base.py
@@ -3,32 +3,36 @@ import os
 python_path = ["modules/sotfet"]
 
 # modules
-decoder_flops = True
+decoder_flops = False
 
 
 bitcell = "sf_cam_bitcell"
 bitcell_array = "sf_cam_bitcell_array"
 wordline_driver = "wordline_driver_array"
 ms_flop_array_horizontal = "ms_flop_array_horizontal"
-ms_flop_array = "ms_flop_array"
+ms_flop_array = "sot_flop_array"
 tag_flop_array = "tag_flop_array"
-ml_precharge_array = "ml_precharge_array"
+ml_precharge_array = "sf_ml_precharge_array"
 ml_precharge = "sf_matchline_precharge"
-body_tap = "cam_body_tap"
+body_tap = "sot_body_tap"
 search_sense_amp_array = "search_sense_amp_array"
 predecoder_flop = "ms_flop_horz_pitch"
+search_sense_amp = "sot_search_sense_amp"
 
+cells_per_group = 2
 
-logic_buffers_height = 1.3
-bitline_buffer_sizes = [2, 8]
-clk_buffers = [2, 6, 18, 32]
+logic_buffers_height = 1.2
+bitline_buffer_sizes = [3, 8]
+clk_buffers = [2, 5.04, 12.7, 32]
 clk_bar_buffers = [2, 6, 18]
-write_buffers = [4, 12]
-chb_buffers = [2, 8]
-wordline_en_buffers = [2, 8]
-sense_amp_buffers = [2, 8]
+write_buffers = [3.5, 12]
+chb_buffers = [2, 3.632, 6.596, 12]
+wordline_en_buffers = [1, 3]
+sense_amp_buffers = [2, 4.9, 12]
 
 wordline_buffers = [1]
+
+ml_precharge_size = 6
 
 # cam config
 word_size = 64

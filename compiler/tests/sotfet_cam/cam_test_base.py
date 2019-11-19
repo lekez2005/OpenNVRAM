12ceee00f95288b88f2f15c07b755d078b3db2a9
ota2 <lekez2005@yahoo.ca>
2019-11-18 19:02:29 -0500

    Implement command line sotfet simulator config

--- compiler/tests/sotfet_cam/cam_test_base.py
+++ compiler/tests/sotfet_cam/cam_test_base.py
@@ -1,13 +1,27 @@
+import os
 import sys
+
+import unittest
 
 sys.path.append('..')
 
 import testutils
-header = testutils.header
+from testutils import parse_args
 
 
 class CamTestBase(testutils.OpenRamTest):
     config_template = "config_sf_cam_{}"
+
+    @staticmethod
+    def run_tests(name):
+        if name == "__main__":
+            if "cmos" in sys.argv:
+                script_dir = os.path.dirname(__file__)
+                sys.path.append(os.path.join(script_dir, "cmos"))
+                CamTestBase.config_template = "config_sw_cam_{}"
+                sys.argv.remove("cmos")
+            parse_args()
+            unittest.main()
 
 
 

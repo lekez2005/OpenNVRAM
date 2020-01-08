8677b572724233e2967ffb52c7d4268e0dc9b9b5
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2020-01-08 12:26:21 -0500

    gdsMill: Fix reader/writer ignoring the 'DATATYPE' of 'PATH'  records
    most readers (like KLayout/Calibre) will not open gds files omiting the
    DATATYPE.
    
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- compiler/gdsMill/gdsMill/gdsPrimitives.py
+++ compiler/gdsMill/gdsMill/gdsPrimitives.py
@@ -31,6 +31,7 @@ class GdsPath:
         self.drawingLayer=""
         self.purposeLayer = None
         self.pathType=""
+        self.dataType=None
         self.pathWidth=""
         self.coordinates=""
         

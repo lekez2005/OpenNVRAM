cb97941717707c5da750452e57bce3ad04ae3e3b
ota2 <lekez2005@yahoo.ca>
2020-11-17 02:28:32 -0500

    Fix write driver bitline mirroring

--- compiler/modules/push_rules/write_driver_array_horizontal.py
+++ compiler/modules/push_rules/write_driver_array_horizontal.py
@@ -1,6 +1,7 @@
 from base.design import design
 from base.hierarchy_layout import GDS_ROT_90
 from base.library_import import library_import
+from base.vector import vector
 from globals import OPTS
 from modules.push_rules.bitcell_aligned_array import bitcell_aligned_array
 
@@ -26,6 +27,13 @@ class WriteDriverArray(bitcell_aligned_array):
     horizontal_pins = ["en", "vdd", "gnd"]
     bus_pins = ["bl", "br", "data", "data_bar", "mask"]
 
+    def connect_mod(self, mod_index):
+        if self.mirror and mod_index % 2 == 1:
+            template = "br[{0}] bl[{0}] data_bar[{0}] data[{0}] en gnd mask vdd"
+        else:
+            template = "bl[{0}] br[{0}] data[{0}] data_bar[{0}] en gnd mask vdd"
+        self.connect_inst(template.format(mod_index).split())
+
     def add_pins(self):
         for i in range(self.word_size):
             self.add_pin("data[{0}]".format(i))
@@ -39,3 +47,19 @@ class WriteDriverArray(bitcell_aligned_array):
         self.add_pin("en")
         self.add_pin("vdd")
         self.add_pin("gnd")
+
+    def add_layout_pins(self):
+        for pin_name in self.child_mod.pins:
+            if pin_name in self.horizontal_pins:
+                for pin in self.child_insts[0].get_pins(pin_name):
+                    self.add_layout_pin(pin_name, pin.layer, offset=vector(0, pin.by()),
+                                        height=pin.height(), width=self.width)
+            elif pin_name in self.bus_pins:
+                for word_index in range(len(self.child_insts)):
+                    conn_index = self.insts.index(self.child_insts[word_index])
+                    pin_index = self.child_mod.pins.index(pin_name)
+                    actual_connection = self.conns[conn_index][pin_index]
+                    self.copy_layout_pin(self.child_insts[word_index], pin_name,
+                                         actual_connection)
+            else:
+                self.copy_layout_pin(self.child_insts[0], pin_name)

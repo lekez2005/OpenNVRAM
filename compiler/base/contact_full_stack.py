33b6631475fc94caafde45edc65af68e7f365356
ota2 <lekez2005@yahoo.ca>
2020-11-30 03:58:15 -0500

    FullStack via fixes
    Make dimension of lowest metal layer configurable by max width given
    Fix start_layer/stop_layer numbers to use those from tech file
    Refactor via definitions to fix bug that resulted in via layers from lower layers being always included

--- compiler/base/contact_full_stack.py
+++ compiler/base/contact_full_stack.py
@@ -1,6 +1,7 @@
 import copy
+import math
 
-from base import design
+from base import design, utils
 from base.vector import vector
 from tech import full_stack_vias
 from base import unique_meta
@@ -26,40 +27,66 @@ class ContactFullStack(design.design, metaclass=unique_meta.Unique):
         return cls._m2_stack
 
     @classmethod
-    def get_name(cls, start_layer=0, stop_layer=-1, centralize=True, dimensions=[]):
-        dim_str = "_".join([str(item) for sublist in dimensions for item in sublist])
-        alignment = "c" if centralize else "l"
-        if stop_layer == -1:
-            stop_layer = "TOP"
-        elif stop_layer == -2:
-            stop_layer = "BOT_TOP"
-        return "full_stack_via_M{}_M{}_{}_{}".format(start_layer + 1, stop_layer, alignment, dim_str)
+    def get_name(cls, start_layer=0, stop_layer=-1, centralize=True, dimensions=None, max_width=None):
 
-    def __init__(self, start_layer=0, stop_layer=-1, centralize=True, dimensions=[]):
-        # TODO: fix bug which creates unnecessary layer vias when start_via is large
-        name = self.get_name(start_layer, stop_layer, centralize, dimensions)
-        design.design.__init__(self, name)
+        dim_str = "_".join([str(item) for sublist in dimensions for item in sublist]) if dimensions else ""
+        width_suffix = "" if not max_width else "_w{:.5g}".format(max_width).replace(".", "__")
+        alignment = "c" if centralize else "l"
+        metal_layers, layer_numbers = utils.get_sorted_metal_layers()
+        stop_layer_name = metal_layers[stop_layer]
+        start_layer_name = metal_layers[start_layer]
+        name = "full_stack_via_{}_{}_{}_{}{}".format(start_layer_name, stop_layer_name, alignment,
+                                                     dim_str, width_suffix)
+        return name
+
+    def __init__(self, start_layer=0, stop_layer=-1, centralize=True, dimensions=None, max_width=None):
+        dimensions = dimensions if dimensions else []
+        design.design.__init__(self, self.name)
 
         via_defs = copy.deepcopy(full_stack_vias)
         for i in range(len(dimensions)):
             via_defs[i]["dimensions"] = dimensions[i]
+
+        metal_layers, layer_numbers = utils.get_sorted_metal_layers()
+        real_start_layer = layer_numbers[start_layer] - 1
+        real_stop_layer = layer_numbers[stop_layer] - 1
+
+        # Remove unused layers and vias
+        for via_def in via_defs:
+            new_vias = []
+            new_layers = []
+            last_layer = None
+            for i in range(len(via_def["vias"])):
+                bottom_layer_num = int(via_def["layers"][i][5:])
+                if real_start_layer < bottom_layer_num <= real_stop_layer:
+                    new_vias.append(via_def["vias"][i])
+                    new_layers.append(via_def["layers"][i])
+                    last_layer = via_def["layers"][i + 1]
+            if last_layer is not None:
+                new_layers.append(last_layer)
+            via_def["vias"] = new_vias
+            via_def["layers"] = new_layers
+
+        via_defs = [via_def for via_def in via_defs if len(via_def["vias"]) > 0]
+        assert len(via_defs) > 0, "Invalid start and stop layers: {}, {}".format(start_layer, stop_layer)
+        if max_width is not None:
+            bottom_via_def = via_defs[0]
+            via_pitch = bottom_via_def["width"] + bottom_via_def["spacing"]
+            total_enclosure = 2 * bottom_via_def["enclosure"][0] + bottom_via_def["width"]
+            num_vias = 1 + math.floor((max_width - total_enclosure) / via_pitch)
+            bottom_via_def["dimensions"] = [num_vias, bottom_via_def["dimensions"][1]]
 
         max_height = max(map(self.get_height, via_defs))
 
         for i in range(len(via_defs)):
             temp_start_layer = 0
             temp_stop_layer = -1
-            if i == 0:
-                temp_start_layer = start_layer
-            elif i == len(via_defs) - 1:
-                temp_stop_layer = stop_layer
-
 
             width, height = self.create_stack(via_defs[i], max_height, centralize, temp_start_layer, temp_stop_layer)
             if i == 0:
                 self.first_layer_height = height
                 self.first_layer_width = width
-            elif i == len(via_defs) - 1:
+            if i == len(via_defs) - 1:
                 self.second_layer_height = height
                 self.second_layer_width = width
         self.height = max_height

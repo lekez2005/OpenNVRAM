5d09e162ebda3f70719370184fdb7b3b82eff8a4
ota2 <lekez2005@yahoo.ca>
2021-05-03 15:48:29 -0400

    1t1s sense amp array data -> dout

--- compiler/modules/shared_decoder/sotfet/sense_amp_br_reset_array.py
+++ compiler/modules/shared_decoder/sotfet/sense_amp_br_reset_array.py
@@ -7,7 +7,7 @@ class sense_amp_br_reset_array(sense_amp_array):
         for i in range(self.word_size):
             self.add_pin("bl[{0}]".format(i))
             self.add_pin("br[{0}]".format(i))
-            self.add_pin("data[{0}]".format(i))
+            self.add_pin("dout[{0}]".format(i))
 
         for pin_name in ["en", "en_bar", "br_reset", "vref", "vdd", "gnd"]:
             self.add_pin(pin_name)

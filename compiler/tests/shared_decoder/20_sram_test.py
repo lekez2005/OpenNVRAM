1b37bd65beaa7cd99935ab38e4d7d45be5d8b58f
ota2 <lekez2005@yahoo.ca>
2021-07-15 23:40:42 -0400

    Break up bank and sram test to enable test inheritance

--- compiler/tests/shared_decoder/20_sram_test.py
+++ compiler/tests/shared_decoder/20_sram_test.py
@@ -1,89 +1,11 @@
-#!/usr/bin/env python
-from importlib import reload
+#!/usr/bin/env python3
 
+from sram_test_base import SramTestBase
 from test_base import TestBase
-import debug
 
 
-class SramTest(TestBase):
-
-    def test_baseline_array(self):
-        from globals import OPTS
-
-        if not OPTS.baseline:
-            return
-
-        # self.sweep_all(cols=[], rows=[128], words_per_row=2, default_col=64, num_banks=1)
-        self.sweep_all()
-
-    def get_sram_class(self):
-        from globals import OPTS
-
-        if hasattr(OPTS, "sram_class"):
-            sram_class = self.load_class_from_opts("sram_class")
-        else:
-            from modules.shared_decoder.cmos_sram import CmosSram
-            from modules.shared_decoder.sotfet.sotfet_mram import SotfetMram
-            if OPTS.mram in ["sotfet", "sot"]:
-                sram_class = SotfetMram
-            else:
-                sram_class = CmosSram
-        import tech
-        tech.drc_exceptions[sram_class.__name__] = tech.drc_exceptions.get("active_density", [])
-        return sram_class
-
-    def sweep_all(self, rows=None, cols=None, words_per_row=None, default_row=64,
-                  default_col=64, num_banks=1):
-        from base import design
-
-        sram_class = self.get_sram_class()
-
-        if rows is None:
-            rows = [16, 32, 64, 128, 256]
-        if cols is None:
-            cols = [32, 64, 128, 256]
-
-        try:
-            col = default_col
-            for row in rows:
-                for words_per_row_ in SramTest.get_words_per_row(col, words_per_row):
-                    reload(design)
-                    self.create_and_test_sram(sram_class, row, col, words_per_row_, num_banks)
-            row = default_row
-            for col in cols:
-                if col == default_col:
-                    continue
-                for words_per_row_ in SramTest.get_words_per_row(col, words_per_row):
-                    reload(design)
-                    self.create_and_test_sram(sram_class, row, col, words_per_row_, num_banks)
-        except ZeroDivisionError as ex:
-            debug.error("Failed {} for row = {} col = {}: {} ".format(
-                sram_class.__name__, row, col, str(ex)), debug.ERROR_CODE)
-            raise ex
-
-    def create_and_test_sram(self, sram_class, num_rows, num_cols, words_per_row, num_banks):
-        debug.info(1, "Test {} row = {} col = {} words_per_row = {} num_banks = {}".
-                   format(sram_class.__name__, num_rows, num_cols, words_per_row, num_banks))
-        word_size = int(num_cols / words_per_row)
-        num_words = num_rows * words_per_row * num_banks
-        a = sram_class(word_size=word_size, num_words=num_words, words_per_row=words_per_row,
-                       num_banks=num_banks, name="sram1", add_power_grid=True)
-
-        self.local_check(a)
-
-    def test_two_dependent_banks(self):
-        from globals import OPTS
-        OPTS.independent_banks = False
-        sram_class = self.get_sram_class()
-        for words_per_row in [1, 2, 4, 8]:
-            self.create_and_test_sram(sram_class, 64, 64, words_per_row=words_per_row, num_banks=2)
-
-    def test_two_independent_banks(self):
-        from globals import OPTS
-        OPTS.independent_banks = True
-        sram_class = self.get_sram_class()
-        for words_per_row in [1, 2, 4, 8]:
-            self.create_and_test_sram(sram_class, 64, 64, words_per_row=words_per_row, num_banks=2)
+class SramTest(SramTestBase, TestBase):
+    pass
 
 
-TestBase.run_tests(__name__)
+SramTest.run_tests(__name__)

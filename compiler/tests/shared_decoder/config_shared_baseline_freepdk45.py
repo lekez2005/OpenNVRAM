43c7641b4412a219fa76546f1de45043ee569c13
ota2 <lekez2005@yahoo.ca>
2021-08-17 04:27:55 -0400

    Timing config for freepdk45 2 SRAM banks

--- compiler/tests/shared_decoder/config_shared_baseline_freepdk45.py
+++ compiler/tests/shared_decoder/config_shared_baseline_freepdk45.py
@@ -11,24 +11,17 @@ write_buffers = [1, 3.42, 11.7, 40]
 
 use_precharge_trigger = False
 
+
 def configure_timing(sram, OPTS):
     num_rows = sram.bank.num_rows
     OPTS.sense_trigger_setup = 0.15
     if num_rows <= 64:
-        if OPTS.num_banks == 1:
-            first_read = 0.5
-            second_read = 0.7
-            OPTS.sense_trigger_delay = second_read - 0.25
-            OPTS.precharge_trigger_delay = 0.6
-            first_write = 0.4
-            second_write = 0.9
-        else:
-            OPTS.sense_trigger_delay = 0.6
-            OPTS.precharge_trigger_delay = 0.6
-            first_read = 0.5
-            second_read = 0.7
-            first_write = 0.4
-            second_write = 0.9
+        first_read = 0.5
+        second_read = 0.7
+        OPTS.sense_trigger_delay = second_read - 0.25
+        OPTS.precharge_trigger_delay = 0.6
+        first_write = 0.4
+        second_write = 0.9
     else:
         if OPTS.num_banks == 1:
             OPTS.precharge_trigger_delay = 1

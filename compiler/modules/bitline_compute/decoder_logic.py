3400a589508543ea6de3ef46b224dbf28f69a631
ota2 <lekez2005@yahoo.ca>
2021-11-18 03:15:48 -0500

    Remove en_0 pin from decoder_logic

--- compiler/modules/bitline_compute/decoder_logic.py
+++ compiler/modules/bitline_compute/decoder_logic.py
@@ -1,7 +1,8 @@
-from base.contact import m1m2, m2m3
+from base.contact import m1m2, m2m3, cross_m2m3
 from base.design import METAL1, METAL2, METAL3
 from base.vector import vector
 from modules.bitcell_vertical_aligned import BitcellVerticalAligned
+from pgates.pinv import pinv
 from pgates.pnand2 import pnand2
 
 
@@ -16,17 +17,17 @@ class DecoderLogic(BitcellVerticalAligned):
 
         self.num_rows = num_rows
 
-        self.en_0_insts = []
-        self.en_1_insts = []
-        self.en_insts = []
+        self.in_0_bar_insts = []
+        self.in_1_bar_insts = []
+        self.en_bar_insts = []
 
         self.add_pins()
         self.create_modules()
         self.calculate_y_offsets()
         self.create_array()
 
-        self.width = self.en_insts[-1].rx()
-        self.height = self.en_insts[-1].uy()
+        self.width = self.en_bar_insts[-1].rx()
+        self.height = self.en_bar_insts[-1].uy()
 
         self.add_layout_pins()
 
@@ -36,9 +37,8 @@ class DecoderLogic(BitcellVerticalAligned):
         for row in range(self.num_rows):
             self.add_pin('in_0[{}]'.format(row))
             self.add_pin('in_1[{}]'.format(row))
-            self.add_pin('out[{}]'.format(row))
+            self.add_pin('out_bar[{}]'.format(row))
 
-        self.add_pin("en_0")
         self.add_pin("en_1")
         self.add_pin_list(["vdd", "gnd"])
 
@@ -47,111 +47,114 @@ class DecoderLogic(BitcellVerticalAligned):
         self.nand = pnand2(size=1, align_bitcell=True, same_line_inputs=True,
                            contact_nwell=False, contact_pwell=False)
         self.add_mod(self.nand)
+        self.inv = pinv(size=1, align_bitcell=True, same_line_inputs=True,
+                        contact_nwell=False, contact_pwell=False)
+        self.add_mod(self.inv)
 
     def create_array(self):
 
         for row in range(self.num_rows):
             y_offset, mirror = self.get_row_y_offset(row)
 
-            self.en_1_insts.append(self.add_inst('en_1_{}'.format(row), mod=self.nand, offset=vector(0, y_offset),
-                                                 mirror=mirror))
-            self.connect_inst(["en_1", "in_1[{}]".format(row), "en_1[{}]".format(row), "vdd", "gnd"])
+            self.in_1_bar_insts.append(self.add_inst('in_1_{}'.format(row), mod=self.nand,
+                                                     offset=vector(0, y_offset), mirror=mirror))
+            self.connect_inst(["en_1", "in_1[{}]".format(row),
+                               "in_1_bar[{}]".format(row), "vdd", "gnd"])
 
-            x_offset = self.en_1_insts[-1].rx()
-            self.en_0_insts.append(self.add_inst('en_0_{}'.format(row), mod=self.nand,
-                                                 offset=vector(x_offset, y_offset), mirror=mirror))
-            self.connect_inst(["en_0", "in_0[{}]".format(row), "en_0[{}]".format(row), "vdd", "gnd"])
+            x_offset = self.in_1_bar_insts[-1].rx()
+            self.in_0_bar_insts.append(self.add_inst('in_0_{}'.format(row), mod=self.inv,
+                                                     offset=vector(x_offset, y_offset),
+                                                     mirror=mirror))
+            self.connect_inst(["in_0[{}]".format(row), "in_0_bar[{}]".format(row), "vdd", "gnd"])
 
-            x_offset = self.en_0_insts[-1].rx()
-            self.en_insts.append(self.add_inst('en_{}'.format(row), mod=self.nand,
-                                               offset=vector(x_offset, y_offset), mirror=mirror))
-            self.connect_inst(["en_0[{}]".format(row), "en_1[{}]".format(row), "out[{}]".format(row), "vdd", "gnd"])
+            x_offset = self.in_0_bar_insts[-1].rx()
+            self.en_bar_insts.append(self.add_inst('en_bar_{}'.format(row), mod=self.nand,
+                                                   offset=vector(x_offset, y_offset), mirror=mirror))
+            self.connect_inst(["in_0_bar[{}]".format(row), "in_1_bar[{}]".format(row),
+                               "out_bar[{}]".format(row), "vdd", "gnd"])
+
+    def get_bot_rail_y(self, row):
+        return self.in_1_bar_insts[row].by() + 0.5 * self.rail_height
+
+    def get_top_rail_y(self, row):
+        return self.in_1_bar_insts[row].uy() - 0.5 * self.rail_height - self.m3_width
 
     def route_layout(self):
-        module_insts = [self.en_1_insts, self.en_0_insts]
-        en_pins = [self.get_pin("en_1"), self.get_pin("en_0")]
+        module_insts = [self.in_1_bar_insts, self.in_0_bar_insts]
+        en_pin = self.get_pin("en_1")
 
         fill_height = self.nand.gate_fill_height
         (fill_height, fill_width) = self.calculate_min_area_fill(fill_height, self.m2_width)
 
         for row in range(self.num_rows):
             for i in range(2):
-                en_pin = en_pins[i]
-                a_pin = module_insts[i][row].get_pin("A")
-                self.add_rect(METAL1, offset=vector(en_pin.lx(), a_pin.cy() - 0.5 * self.m1_width),
-                              width=a_pin.lx() - en_pin.lx())
-                self.add_contact_center(m1m2.layer_stack, offset=vector(en_pin.cx(), a_pin.cy()))
+                if i == 0:
+                    # connect enable pin
+                    a_pin = module_insts[i][row].get_pin("A")
+                    self.add_rect(METAL1, offset=vector(en_pin.lx(),
+                                                        a_pin.cy() - 0.5 * self.m1_width),
+                                  width=a_pin.lx() - en_pin.lx())
+                    self.add_contact_center(m1m2.layer_stack, offset=vector(en_pin.cx(), a_pin.cy()))
+                    pin_name = "B"
+                else:
+                    pin_name = "A"
+                in_pin = module_insts[i][row].get_pin(pin_name)
+                self.add_contact_center(m1m2.layer_stack, offset=in_pin.center(), rotate=90)
+                self.add_contact_center(m2m3.layer_stack, offset=in_pin.center())
 
-                b_pin = module_insts[i][row].get_pin("B")
-                self.add_contact_center(m1m2.layer_stack, offset=b_pin.center())
-                self.add_contact_center(m2m3.layer_stack, offset=b_pin.center())
-
-                self.add_rect_center(METAL2, offset=b_pin.center(), width=fill_width,
+                self.add_rect_center(METAL2, offset=in_pin.center(), width=fill_width,
                                      height=fill_height)
 
-                if row % 2 == 0:
-                    y_offset = module_insts[i][row].by() + 0.5 * self.rail_height
-                else:
-                    y_offset = module_insts[i][row].uy() - 0.5 * self.rail_height - self.m3_width
-                self.add_rect(METAL3, offset=vector(b_pin.lx(), b_pin.cy()), height=y_offset - b_pin.cy())
-
                 if i == 0:
-                    self.add_layout_pin("in_1[{}]".format(row), METAL3, offset=vector(0, y_offset),
-                                        width=b_pin.rx())
+                    pin_name = f"in_1[{row}]"
+                    y_offset = self.get_bot_rail_y(row)
                 else:
-                    self.add_layout_pin("in_0[{}]".format(row), METAL3, offset=vector(b_pin.lx(), y_offset),
-                                        width=self.width - b_pin.lx())
+                    pin_name = f"in_0[{row}]"
+                    y_offset = self.get_top_rail_y(row)
+                self.add_rect(METAL3, offset=vector(in_pin.cx() - 0.5 * self.m3_width, in_pin.cy()),
+                              height=y_offset - in_pin.cy())
 
-                self.route_en_0_nand_output(row)
-                self.route_en_1_nand_output(row, fill_width, fill_height)
+                self.add_layout_pin(pin_name, METAL3, offset=vector(0, y_offset),
+                                    width=in_pin.cx() + 0.5 * self.m3_width)
+            self.route_en_0_output(row)
+            self.route_en_1_output(row, fill_width, fill_height)
 
-    def route_en_0_nand_output(self, row):
+    def route_en_0_output(self, row):
         # route en_0 output to en_insts a pin
-        a_pin = self.en_insts[row].get_pin("A")
-        z_pin = self.en_0_insts[row].get_pin("Z")
+        a_pin = self.en_bar_insts[row].get_pin("A")
+        z_pin = self.in_0_bar_insts[row].get_pin("Z")
         self.add_rect(METAL1, offset=vector(z_pin.rx(), a_pin.cy() - 0.5 * self.m1_width),
                       width=a_pin.lx() - z_pin.rx())
 
-    def route_en_1_nand_output(self, row, fill_width, fill_height):
+    def route_en_1_output(self, row, fill_width, fill_height):
         # route en_1 output to en_insts b pin
-        z_pin = self.en_1_insts[row].get_pin("Z")
-        b_pin = self.en_insts[row].get_pin("B")
-        if row % 2 == 1:
-            y_offset = self.en_insts[row].by() + 0.5 * self.rail_height
-            m2_start = z_pin.by()
-            self.add_contact(m2m3.layer_stack, offset=vector(z_pin.lx(), y_offset))
+        z_pin = self.in_1_bar_insts[row].get_pin("Z")
+        b_pin = self.en_bar_insts[row].get_pin("B")
 
-        else:
-            y_offset = self.en_insts[row].uy() - 0.5 * self.rail_height - self.m3_width
-            m2_start = z_pin.uy()
-            self.add_contact(m2m3.layer_stack, offset=vector(z_pin.lx(),
-                                                             y_offset + self.m2_width - m2m3.second_layer_height))
+        y_offset = self.get_bot_rail_y(row)
+        x_offset = z_pin.cx() - 0.5 * self.m2_width
+        self.add_rect(METAL2, offset=vector(x_offset, y_offset), height=z_pin.by() - y_offset)
+        self.add_cross_contact_center(cross_m2m3, vector(z_pin.cx(), y_offset + 0.5 * self.m3_width))
 
-        self.add_rect(METAL2, offset=vector(z_pin.lx(), m2_start), height=y_offset - m2_start)
-
-        self.add_rect(METAL3, offset=vector(z_pin.lx(), y_offset), width=b_pin.rx() - z_pin.lx())
-        self.add_rect(METAL3, offset=vector(b_pin.cx() - 0.5 * self.m3_width, b_pin.cy()),
-                      height=y_offset - b_pin.cy())
-        self.add_contact_center(m1m2.layer_stack, offset=b_pin.center())
+        self.add_rect(METAL3, offset=vector(x_offset, y_offset), width=b_pin.cx() - x_offset)
+        self.add_rect(METAL3, offset=vector(b_pin.cx() - 0.5 * self.m3_width, y_offset),
+                      height=b_pin.cy() - y_offset)
+        self.add_contact_center(m1m2.layer_stack, offset=b_pin.center(), rotate=90)
         self.add_contact_center(m2m3.layer_stack, offset=b_pin.center())
 
         self.add_rect_center(METAL2, offset=b_pin.center(), width=fill_width,
                              height=fill_height)
 
     def add_layout_pins(self):
-        # enable pins
-        pin_names = ["en_0", "en_1"]
-        module_insts = [self.en_0_insts[0], self.en_1_insts[1]]
-        for i in range(2):
-            module_inst = module_insts[i]
-            x_offset = module_inst.lx() + self.m1_space
-            self.add_layout_pin(pin_names[i], METAL2, offset=vector(x_offset, 0),
-                                height=self.height)
+        # enable pin
+        x_offset = self.in_1_bar_insts[0].lx() + self.m1_space
+        self.add_layout_pin("en_1", METAL2, offset=vector(x_offset, 0),
+                            height=self.height)
 
         for row in range(self.num_rows):
-            self.copy_layout_pin(self.en_insts[row], "Z", "out[{}]".format(row))
-            self.copy_layout_pin(self.en_insts[row], "vdd", "vdd".format(row))
-            self.copy_layout_pin(self.en_insts[row], "gnd", "gnd".format(row))
+            self.copy_layout_pin(self.en_bar_insts[row], "Z", "out_bar[{}]".format(row))
+            self.copy_layout_pin(self.en_bar_insts[row], "vdd", "vdd".format(row))
+            self.copy_layout_pin(self.en_bar_insts[row], "gnd", "gnd".format(row))
 
     def add_body_taps(self):
-        self._add_body_taps(self.en_1_insts[0], self.en_1_insts)
+        self._add_body_taps(self.in_1_bar_insts[0], self.in_1_bar_insts)

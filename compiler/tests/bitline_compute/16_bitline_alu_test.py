331311215bbc33ec0e9604ca4514fee955e1591d
ota2 <lekez2005@yahoo.ca>
2022-11-01 03:21:38 -0400

    Set word size based on ALU word size

--- compiler/tests/bitline_compute/16_bitline_alu_test.py
+++ compiler/tests/bitline_compute/16_bitline_alu_test.py
@@ -16,8 +16,9 @@ class BitlineAluTest(TestBase):
         OPTS.run_optimizations = False
 
         num_words = 32
-        words_per_row = 2
-        word_size = 16
+        words_per_row = 1
+        word_size = 256
+        alu_word_size = 32
         num_cols = word_size * words_per_row
 
         bank = BlBank(name="bank1", word_size=num_cols, num_words=num_words, words_per_row=1)
@@ -25,7 +26,8 @@ class BitlineAluTest(TestBase):
         if OPTS.serial:
             a = BitSerialALU(bank=bank, num_cols=num_cols)
         else:
-            a = BitlineALU(bank=bank, num_cols=num_cols, word_size=word_size, cells_per_group=1)
+            a = BitlineALU(bank=bank, num_cols=num_cols, word_size=alu_word_size,
+                           cells_per_group=OPTS.alu_cells_per_group)
 
         self.local_check(a)
 

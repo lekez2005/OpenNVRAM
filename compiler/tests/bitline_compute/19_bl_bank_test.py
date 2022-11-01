8721b14a538038c4d1d3d12964e7439180e8575b
ota2 <lekez2005@yahoo.ca>
2022-11-01 03:22:52 -0400

    Add taps to decoder logic to pass DRC/LVS tests

--- compiler/tests/bitline_compute/19_bl_bank_test.py
+++ compiler/tests/bitline_compute/19_bl_bank_test.py
@@ -6,6 +6,10 @@ from bank_test_base import BankTestBase
 
 class BlBankTest(BankTestBase, TestBase):
     def local_check(self, a, final_verification=False):
+        from globals import OPTS
+        if OPTS.baseline:
+            super().local_check(a, final_verification)
+            return
         if hasattr(a.decoder_logic, "add_body_taps"):
             a.decoder_logic.add_body_taps()
         TestBase.local_check(self, a, final_verification)

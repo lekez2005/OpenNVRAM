c3e8a7f17586cd8d7c3ba9263200655922b0d2a8
ota2 <lekez2005@yahoo.ca>
2022-11-01 03:23:21 -0400

    Configure modules before creating SRAM class

--- compiler/tests/bitline_compute/20_bl_sram_test.py
+++ compiler/tests/bitline_compute/20_bl_sram_test.py
@@ -5,7 +5,11 @@ from sram_test_base import SramTestBase
 
 
 class BlSramTest(SramTestBase, TestBase):
-    pass
+    def get_sram_class(self):
+        from globals import OPTS
+        OPTS.configure_modules(None, OPTS)
+        sram_class = self.load_class_from_opts("sram_class")
+        return sram_class
 
 
 BlSramTest.run_tests(__name__)

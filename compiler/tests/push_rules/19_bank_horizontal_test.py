4c3d840f0d7a600f4edac4c56df98a57aa04a353
ota2 <lekez2005@yahoo.ca>
2022-12-11 03:46:37 -0500

    Refactor base test import

--- compiler/tests/push_rules/19_bank_horizontal_test.py
+++ compiler/tests/push_rules/19_bank_horizontal_test.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python
-import os
-import sys
 
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
-# insert at beginning of path to override current directory's test_base
-sys.path.insert(0, os.path.join(parent_dir, "mram"))
-bank_test = __import__("19_bank_test")
+from test_base import TestBase
+from bank_test_base import BankTestBase
 
 
-class BankTest(bank_test.BankTest):
-    config_template = "config_push_hs_{}"
-
-    def setUp(self):
-        super().setUp()
-        from globals import OPTS
-        OPTS.baseline = True
-
+class BankTest(BankTestBase, TestBase):
     @staticmethod
     def get_bank_class():
         from modules.push_rules.horizontal_bank import HorizontalBank
